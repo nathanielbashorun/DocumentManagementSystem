77d97f0e021a4671a4d1253e696628d59ff7c9ed
Oladotun Bashorun <61381276+nathanielbashorun@users.noreply.github.com>
2020-10-09 14:36:54 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
     // Connection strings to the database using PDO(PHP Data Objects)
-    $db_user = 'dotman';
-    $db_pass = 'timilehin';
+    $db_user = '';
+    $db_pass = '';
     $db_name = 'documentmanagementsystem';
 
     $db = new PDO('mysql:host=localhost;dbname=' . $db_name . ';charset=utf8',$db_user,$db_pass);
